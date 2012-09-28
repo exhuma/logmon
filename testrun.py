9ba1078f5fa960542d39a5b2a7d35a9805ccda6c
Michel Albert <michel@albert.lu>
2012-09-28 07:53:03 +0200

    The testrunner now takes user-input.

--- testrun.py
+++ testrun.py
@@ -1,15 +1,22 @@
+from cmd import Cmd
 import logging
+import sys
 LOG = logging.getLogger(__name__)
-logging.getLogger("a")
-logging.getLogger("b")
-logging.getLogger("c")
-logging.getLogger("d").info("inf Hello World!")
-logging.getLogger("e")
-logging.getLogger("f")
 
-from time import sleep
-sleep(3)
-l = logging.getLogger('x')
-sleep(1)
-l.warning('warn')
-sleep(3)
+
+class MyCmd(Cmd):
+
+    def do_EOF(self, line):
+        LOG.warning("Exiting")
+        print ""
+        sys.exit(0)
+
+    def do_hello(self, line):
+        LOG.info("Hello")
+
+    do_exit = do_EOF
+
+if __name__ == '__main__':
+
+    app = MyCmd()
+    app.cmdloop()
