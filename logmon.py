fbd11f1022ed53993f101fb509cf59bc93fd0820
Michel Albert <michel@albert.lu>
2012-09-28 08:09:19 +0200

    Added option to select UI and to daemonize it.

--- logmon.py
+++ logmon.py
@@ -34,43 +34,66 @@ def run_monitor():
     MON.start()
 
 
-def execute_script():
+def parse_options():
     usage = "logmon.py scriptfile [arg] ..."
     parser = OptionParser(usage=usage)
     parser.allow_interspersed_args = False
+    parser.add_option('-d', '--daemonize', action='store_true', default=False,
+        help='Run the monitor application as daemon. This will make it '
+            'exit automatically once the monitored application finishes. For '
+            'the graphical window, you may not want this to happen!')
+    parser.add_option('-u', '--ui', default='tk', metavar='UI',
+        help='Select the user interface to run. Currently only "tk" is '
+            'supported.')
 
     if not sys.argv[1:]:
         parser.print_usage()
         sys.exit(2)
 
     (options, args) = parser.parse_args()
+
+    if not args:
+        parser.print_usage()
+        sys.exit(2)
+
+    return options, args
+
+
+def execute_script(options, args):
+
     sys.argv[:] = args
 
-    if len(args) > 0:
-        progname = args[0]
-        sys.path.insert(0, os.path.dirname(progname))
-        with open(progname, 'rb') as fp:
-            code = compile(fp.read(), progname, 'exec')
-        globs = {
-            '__file__': progname,
-            '__name__': '__main__',
-            '__package__': None,
-        }
-        exec code in globs, None
-    else:
-        parser.print_usage()
-    return parser
+    progname = args[0]
+    sys.path.insert(0, os.path.dirname(progname))
+    with open(progname, 'rb') as fp:
+        code = compile(fp.read(), progname, 'exec')
+    globs = {
+        '__file__': progname,
+        '__name__': '__main__',
+        '__package__': None,
+    }
+    exec code in globs, None
+
+
+def run_ui(options, args):
+    if options.ui not in ('tk', ):
+        print "{0} is an unsupported UI!".format(options.ui)
+        sys.exit(2)
+
+    if options.ui.lower().strip() == 'tk':
+        import localui
+        t = threading.Thread(target=localui.main, args=(QUEUE,))
+        if options.daemonize:
+            t.daemon = True
+        t.start()
 
 
 def main():
+    options, args = parse_options()
     run_monitor()
-    execute_script()
+    run_ui(options, args)
+    execute_script(options, args)
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    import localui
-    t = threading.Thread(target=localui.main, args=(QUEUE,))
-    t.daemon = True
-    t.start()
     main()
